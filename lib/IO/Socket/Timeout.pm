a46a81a703ec06a0790d35b542cdaa07166196d8
Damien Krotkine <dams@cpan.org>
2013-12-24 03:00:54 +0000

    Import of DAMS/IO-Socket-Timeout-0.21 from CPAN.
    gitpan-cpan-distribution: IO-Socket-Timeout
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         DAMS/IO-Socket-Timeout-0.21.tar.gz
    gitpan-cpan-author:       DAMS
    gitpan-cpan-maturity:     released

--- lib/IO/Socket/Timeout.pm
+++ lib/IO/Socket/Timeout.pm
@@ -8,7 +8,7 @@
 #
 package IO::Socket::Timeout;
 {
-  $IO::Socket::Timeout::VERSION = '0.20';
+  $IO::Socket::Timeout::VERSION = '0.21';
 }
 
 use strict;
@@ -169,7 +169,7 @@ BEGIN {
 
 package IO::Socket::Timeout::Role::SetSockOpt;
 {
-  $IO::Socket::Timeout::Role::SetSockOpt::VERSION = '0.20';
+  $IO::Socket::Timeout::Role::SetSockOpt::VERSION = '0.21';
 }
 use Carp;
 use Socket;
@@ -229,7 +229,7 @@ sub _set_sock_opt {
 
 package IO::Socket::Timeout::Role::PerlIO;
 {
-  $IO::Socket::Timeout::Role::PerlIO::VERSION = '0.20';
+  $IO::Socket::Timeout::Role::PerlIO::VERSION = '0.21';
 }
 use PerlIO::via::Timeout;
 
@@ -251,7 +251,7 @@ IO::Socket::Timeout - IO::Socket with read/write timeout
 
 =head1 VERSION
 
-version 0.20
+version 0.21
 
 =head1 SYNOPSIS
 
